7bace9b03550908630f1147125f998e73a1fbc41
Udit <royalboy0012@gmail.com>
2023-06-21 11:38:03 +0530

    added port shifting

--- src/components/MiscComp/Card.js
+++ src/components/MiscComp/Card.js
@@ -4,7 +4,7 @@ import styles from './Card.module.css'
 const Card = (props) => {
     const {title,route,description} =props.moduleList
   return (
-    <Link to={`/${route}`} className={styles.card}>
+    <Link to={`http://localhost:3001`} target='_blank' className={styles.card}>
         <div className={styles.title}>{title}</div>
         <div className={styles.description}>{description}</div>
     </Link>
